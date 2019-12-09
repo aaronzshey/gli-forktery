29c28432a9ad3a73aa894395d97b38fb02b0c215
Glitch (hello-express) <none>
2019-12-09 18:35:06 +0000

    💸🐝 Checkpoint ./public/client.js:234254/916

--- public/client.js
+++ public/client.js
@@ -1,8 +1,9 @@
 // Create WebSocket connection.
-function openSocket() {
+function openSocket(messageHandler) {
   console.log('connecting to server...');
   // replace the 'https' in the url with the secure websocket protocol.
-  const socket = new WebSocket(window.location.origin.replace(/^https/, 'wss'));
+  let url = window.location.origin.replace(/^https/, 'wss');
+  const socket = new WebSocket(url);
   
   // Connection opened
   socket.addEventListener('open', function (event) {
@@ -14,7 +15,7 @@ function openSocket() {
   socket.addEventListener('close', function (event) {
     console.log('disconnected');
     // the server went away, try re-connecting in 5 seconds.
-    setTimeout(openSocket, 5000);
+    setTimeout(openSocket, 2000);
   });
 
   // Listen for messages
@@ -23,17 +24,21 @@ function openSocket() {
     try {
       // this example expects every message to be in JSON format.
       data = JSON.parse(event.data);
+      messageHandler(data);
     } catch (e) {
       console.warn('invalid message from the server!');
       return;
     }
-    console.log('message:', data);
-    // if it's a message about the client count, update the elements
-    if (data.type === 'count') {
-      document.querySelector('.count').innerText = data.count;
-    }
   });
 }
 
+function onMessage(data) {
+  console.log('message:', data);
+  // if it's a message about the client count, update the elements
+  if (data.type === 'count') {
+    document.querySelector('.count').innerText = data.count;
+  }
+}
+
 // open a connection when the script is loaded
-openSocket();
+openSocket(onMessage);
