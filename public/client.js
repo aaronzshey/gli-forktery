7d1108baf4fe917f5de427bd6de0b82863612d33
Glitch (hello-express) <none>
2019-12-09 18:59:05 +0000

    😋🐺 Checkpoint ./public/client.js:234254/4209

--- public/client.js
+++ public/client.js
@@ -1,59 +1,73 @@
-class FriendlyWebSocket extends EventTarget {
-  constructor ({ path='/' } = {}) {
+class FriendlyWebSocket {
+  constructor({ path='/' } = {}) {
+    this.path = path;
+    this.connect();
+    this.connected = false;
+    this.messageHandlers = new Set()
+  }
+  
+  connect() {
+    let url = window.location.origin.replace(/^https/, 'wss') + this.path;
+    console.log(url);
     this.socket = new WebSocket(url);
+    
+    // Connection opened
+    this.socket.addEventListener('open', function (event) {
+      console.log('connected!');
+      this.connected = true;
+      // this isn't necessary, but it's polite to say hi!
+      this.socket.send('Hello Server!');
+    });
+
+    this.socket.addEventListener('close', function (event) {
+      console.log('disconnected');
+      this.connected = false;
+      // the server went away, try re-connecting in 5 seconds.
+      setTimeout(() => this.connect(), 2000);
+    });
+
+    // Listen for messages
+    this.socket.addEventListener('message', function (event) {
+      // tell the listeners about it
+      this.messageHandlers.forEach(handler => {
+        // don't let one listener spoil the batch
+        try {
+          handler(event.data)
+        } catch (e) {
+          console.warn('error in message handler', e);
+        }
+      });
+    });
+  }
+  
+  on(type, handler) {
+    if (type === 'message') {
+      this.messageHandlers.add(handler);
+    } 
+  }
+  
+  off(type, handler) {
+    if (type === 'message') {
+      this.messageHandlers.delete(handler);
+    } 
+  }
+  
+  send(message) {
+    if (this.connected) {
+      this.socket.send(message);
+    }
   }
 }
 
-// Create WebSocket connection.
-function openSocket(messageHandler) {
-  console.log('connecting to server...');
-  // replace the 'https' in the url with the secure websocket protocol.
-  let url = window.location.origin.replace(/^https/, 'wss');
-  const socket = new WebSocket(url);
-  let connected = false;
-  
-  // Connection opened
-  socket.addEventListener('open', function (event) {
-    console.log('connected!');
-    connected = true;
-    // this isn't necessary, but it's polite to say hi!
-    socket.send('Hello Server!');
-  });
-
-  socket.addEventListener('close', function (event) {
-    console.log('disconnected');
-    connected = false;
-    // the server went away, try re-connecting in 5 seconds.
-    setTimeout(openSocket, 2000);
-  });
-
-  // Listen for messages
-  socket.addEventListener('message', function (event) {
-    let data;
-    try {
-      // this example expects every message to be in JSON format.
-      data = JSON.parse(event.data);
-      messageHandler(data);
-    } catch (e) {
-      console.warn('invalid message from the server!');
-      return;
-    }
-  });
-  
-  // many services require a periodic message to keep a websocket open
-  setInterval(function () {
-    if (connected) {
-      socket.send('heartbeat');
-    }
-  }, 5000);
-  
-  return {
-    send: message => socket.send(message)
-  };
-}
 
 function onMessage(data) {
   console.log('message:', data);
+  try {
+    // this example expects every message to be in JSON format.
+    data = JSON.parse(event.data);
+  } catch (e) {
+    console.warn('invalid message from server', )
+  }
   // if it's a message about the client count, update the elements
   if (data.type === 'count') {
     document.querySelector('.count').innerText = data.count;
