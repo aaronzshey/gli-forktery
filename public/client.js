6d900a2048ccf999e9dcd6e3af2bcfd162be5328
Glitch (hello-express) <none>
2019-12-09 20:53:56 +0000

    🎢🎰 Checkpoint ./public/client.js:234254/4924

--- public/client.js
+++ public/client.js
@@ -1,57 +1,63 @@
 class FriendlyWebSocket {
-  constructor({ path='/' } = {}) {
+  constructor({ path = "/" } = {}) {
     this.path = path;
     this.connect();
     this.connected = false;
     this.messageHandlers = new Set();
   }
-  
+
   connect() {
-    let url = window.location.origin.replace(/^https/, 'wss') + this.path;
+    let url;
+    if (location.protocol === 'https:') {
+      url = 'ws://' + location.host + this.path;
+    } else {
+      url = 'ws://' + location.host + this.path;
+    }
+    = window.location.origin.replace(/^https/, "wss") + this.path;
     console.log(url);
     this.socket = new WebSocket(url);
-    
+
     // Connection opened
-    this.socket.addEventListener('open', (event) => {
-      console.log('connected!');
+    this.socket.addEventListener("open", event => {
+      console.log("connected!");
       this.connected = true;
       // this isn't necessary, but it's polite to say hi!
-      this.socket.send('Hello Server!');
+      this.socket.send("Hello Server!");
     });
 
-    this.socket.addEventListener('close', (event) => {
-      console.log('disconnected');
+    this.socket.addEventListener("close", event => {
+      console.log("disconnected");
       this.connected = false;
       // the server went away, try re-connecting in 5 seconds.
       setTimeout(() => this.connect(), 2000);
     });
 
     // Listen for messages
-    this.socket.addEventListener('message', (event) => {
+    this.socket.addEventListener("message", event => {
       // tell the listeners about it
       this.messageHandlers.forEach(handler => {
         // don't let one listener spoil the batch
         try {
-          handler(event.data)
+          handler(event.data);
         } catch (e) {
-          console.warn('error in message handler', e);
+          console.warn("error in message handler", e);
         }
       });
     });
   }
-  
+
   on(type, handler) {
-    if (type === 'message') {
+    if (type === "message") {
       this.messageHandlers.add(handler);
-    } 
+    }
   }
-  
+
   off(type, handler) {
-    if (type === 'message') {
+    if (type === "message") {
       this.messageHandlers.delete(handler);
-    } 
+    }
   }
-  
+
   send(message) {
     if (this.connected) {
       this.socket.send(message);
@@ -60,19 +66,17 @@ class FriendlyWebSocket {
 }
 
 
-function onMessage(data) {
-  console.log('message:', data);
+let s = new FriendlyWebSocket();
+s.on("message", data => {
+  console.log("message:", data);
   try {
     // this example expects every message to be in JSON format.
     data = JSON.parse(event.data);
   } catch (e) {
-    console.warn('invalid message from server', data);
+    console.warn("invalid message from server", data);
   }
   // if it's a message about the client count, update the elements
-  if (data.type === 'count') {
-    document.querySelector('.count').innerText = data.count;
+  if (data.type === "count") {
+    document.querySelector(".count").innerText = data.count;
   }
-}
-
-let s = new FriendlyWebSocket();
-s.on('message', onMessage);
+});
