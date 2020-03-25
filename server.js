c448c04815998ba29a70b651f236713e10563ded
Glitch (hello-express) <none>
2020-03-25 05:26:41 +0000

    😀👓 Checkpoint ./server.js:2663914/2793

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const http = require('http');
-const express = require('express');
-const ws = require('ws');
+const http = require("http");
+const express = require("express");
+const ws = require("ws");
 
 const app = express();
 
@@ -13,40 +13,44 @@ const wss = new ws.Server({ server });
 let sockets = new Set();
 
 // based on https://www.npmjs.com/package/ws#simple-server
-wss.on('connection', function connection(ws) {
+wss.on("connection", function connection(ws) {
   sockets.add(ws);
 
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
   });
-  
-  ws.on('close', function () {
+
+  ws.on("close", function() {
     sockets.delete(ws);
     // tell everyone a client left
     update();
   });
-  
+
   // tell everyone a client joined
   update();
 });
 
 function update() {
   // send an updated client count to every open socket.
-  sockets.forEach(ws => ws.send(JSON.stringify({
-    type: 'count',
-    count: sockets.size
-  })));
+  sockets.forEach(ws =>
+    ws.send(
+      JSON.stringify({
+        type: "count",
+        count: sockets.size
+      })
+    )
+  );
 }
- 
+
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
 });
 
 // listen for requests!
 const listener = server.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
