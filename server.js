8b182fea69d7e282f718211067d2a9de306cde00
Glitch (hello-express) <none>
2020-03-27 23:27:31 +0000

    👨🏭 Checkpoint ./server.js:2663914/2520 ./watch.json:2663914/13

--- server.js
+++ server.js
@@ -2,18 +2,29 @@ const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const MongoClient = require("mongodb").MongoClient
+const MongoClient = require("mongodb").MongoClient;
+const database =
+  "mongodb+srv://mongoGod:mongopass!@demo-cluster-dctcz.mongodb.net/test?retryWrites=true&w=majority";
+var db;
+var client;
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.listen("3000", () => {
-  console.log("Now listening!");
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html")
+  res.sendFile(__dirname + "/index.html");
 });
 
-
+MongoClient.connect(database, (err, database) => {
+  if (err) {
+    return console.log(err);
+  } 
+    db = client.db("demo-cluster")
+    app.listen(3000, () => {
+      console.log("Listening on port 3000")
+    })
+  
+});
 app.post("/quotes", (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
 });
