d74d86fd93940b2d760e9cd54f099128ccbecf6d
Glitch (hello-express) <none>
2020-03-28 04:47:30 +0000

    👵👍 Checkpoint ./server.js:2663914/2599 ./watch.json:2663914/132

--- server.js
+++ server.js
@@ -4,29 +4,37 @@ const bodyParser = require("body-parser");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 var database;
-var db;
+let db;
 var client;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-/*
-MongoClient.connect(database, (err, database) => {
-  if (err) {
-    return console.log(err);
-  } 
-    db = client.db("demo-cluster")
+MongoClient.connect(
+  "mongodb+srv://mongoGod:mongopass%21@demo-cluster-dctcz.mongodb.net/test?retryWrites=true&w=majority ",
+  (err, database) => {
+    if (err) {
+      return console.log(err);
+    }
+    db = client.db("demo-cluster");
     app.listen(3000, () => {
-      console.log("Listening on port 3000")
-    })
-  
-});
-*/
+      console.log("Listening on port 3000");
+    });
+  }
+);
 
 app.post("/quotes", (req, res) => {
   console.log(req.body);
 });
+
+/* 
+mongodb+srv://mongoGod:mongopass!@ demo-cluster-dctcz.mongodb.net/test ?retryWrites=true&w=majority 
+mongodb://[dbuser:dbpassword@]host:port/dbname
+*/
