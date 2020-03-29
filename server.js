0bd84e4d1842f7c7c885af7f3dd6b78c1879ee11
Glitch (gli-forktery) <none>
2020-03-29 19:49:52 +0000

    🛬🎲 Checkpoint ./server.js:2663914/5007 ./watch.json:2663914/42

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express(); 
+const app = express();
 const MongoClient = require("mongodb").MongoClient;
 var database;
 
@@ -11,19 +11,21 @@ var listener = app.listen("8080", function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
-
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 var db;
-var uri = `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@${process.env.MONGO_TARGET}/test?retryWrites=true&w=majority`
-console.log(uri)
-/*
-MongoClient.connect(uri, { useUnifiedTopology: true } , 
-  (err, client) => {
-    if (err) {
-      return console.log(err);
+const uri = `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@${process.env.MONGO_TARGET}/test?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(
+  uri,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+client.connect(err => {
+  if (err) {
+    return console.log(err);
     db = client.db(process.env.DB_NAME);
     app.listen(3000, () => {
       console.log("Listening on port 3000");
@@ -33,7 +35,7 @@ MongoClient.connect(uri, { useUnifiedTopology: true } ,
 
 app.post("/quotes", (req, res) => {
   console.log(req.body);
-}); 
+});
 
 /*
 mongodb+srv://mongoGod:mongopass!@ demo-cluster-dctcz.mongodb.net/test ?retryWrites=true&w=majority 
