158847cead0fb53c8524560914e82d4a446961d3
Glitch (gli-forktery) <none>
2020-05-31 05:07:06 +0000

    🦀🐴 Checkpoint ./index.ejs:2663914/201 ./styles.css:2663914/250 ./index.html:2663914/537 ./server.js:2663914/695 ./package.json:2663914/4494

--- server.js
+++ server.js
@@ -3,4 +3,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 30000")
+})
