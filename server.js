16e1b090de0e28bbb965138970c790767d5519b8
Glitch (gli-forktery) <none>
2020-06-01 18:10:24 +0000

    🌔🚘 Checkpoint ./server.js:2663914/49

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"))
 
 app.get("/", (req, res) => {
   res.render("index");
