a91e9de89bef0df9284ebe3e353b427eb96df4c9
Glitch (gli-forktery) <none>
2020-06-05 05:44:30 +0000

    💨🎍 Checkpoint ./server.js:2663914/2549

--- server.js
+++ server.js
@@ -4,7 +4,23 @@ const bodyParser = require("body-parser");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
+
+const uri = `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@${process.env.MONGO_TARGET}/test?retryWrites=true&w=majority`;
+console.log(uri)
+
+/* 
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+
+client.connect(err => {
+  if (err) return console.error(err);
+  console.log("Connected to server");
+});
+*/
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -14,7 +30,7 @@ app.listen(3000, () => {
   console.log("Listening on port 30000");
 });
 
-app.post("/posts/input", async (req, res) => {
-  await req.body
-  console.log(req.body)
-})
+app.post("/posts/input", (req, res) => {
+  req.body;
+  console.log(req.body);
+});
